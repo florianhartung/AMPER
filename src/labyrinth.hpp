380f434e5f1ed9adeaeedde9ff1f7a6413c6a4b4
Jakob <jakobkampers23@gmail.com>
2024-06-10 13:06:31 +0000

    feat: definition of labyrinth and path

---
+++ src/labyrinth.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "types.hpp"
+
+const static Labyrinth labyrinth ({
+    false, false, true, false,
+    false, true, true, false,
+    false, false, true, false,
+    false, false, true, false,
+});  
---
+++ src/types.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream> 
+#include <array>
+#include <queue> 
+
+constexpr size_t LABYRINTH_SIZE = 4;
+
+/**
+ * @brief datatype for the labyrinth
+ * holds an array of bools (true: block, false: air)
+ * the array holds 2d map, column first
+*/
+class Labyrinth {
+public: 
+    Labyrinth(const std::array<bool, LABYRINTH_SIZE * LABYRINTH_SIZE> &labyrinth);
+
+    bool get(size_t x, size_t y) const; 
+
+private: 
+    std::array<bool, LABYRINTH_SIZE * LABYRINTH_SIZE> m_cells = {}; 
+}; 
+
+
+struct PathNode {
+    size_t x; 
+    size_t y; 
+};
+
+/**
+ * @brief basically a FIFO queue holding the path
+*/
+class Path {
+public: 
+    Path() = default; 
+
+    // the node is emplaced at back of the path
+    void pushNode(size_t x, size_t y);
+    // the first node will be removed and returned 
+    PathNode popNode(); 
+
+private: 
+    std::deque<PathNode> m_nodes {}; 
+
+};
+
+
