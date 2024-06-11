d0654a713d8214cfac4aabc0f3a050efa1385b1e
Florian Hartung <florian.hartung5+github@gmail.com>
2024-06-11 15:38:38 +0200

    feat(controller): execute pathfinding

--- src/controller/robot_controller.cpp
+++ src/controller/robot_controller.cpp
@@ -1,9 +1,18 @@
 #include "ros/ros.h"
+#include "path_finding.hpp"
+
+
+const std::tuple<size_t, size_t> START_POS(0, 0);
+const std::tuple<size_t, size_t> END_POS(1, 1);
+
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "robot_controller");
+	ROS_INFO("Started controller");
 
-	ROS_INFO("Hello world from controller");
+
+	const Path path = findPath(LABYRINTH, START_POS, END_POS);
+
 
 	ros::spin();
 	return 0;
