df1761d2e252ebb3b4c7c59f88313ee31516862e
Visar <visarlumi77@gmail.com>
2024-06-30 20:33:10 +0200

    fix(build): v0.97

--- src/AMPER/src/controller/const_labyrinth.hpp
+++ src/AMPER/src/controller/const_labyrinth.hpp
@@ -1,19 +1,19 @@
 #pragma once
-#include <array>
-#include "labyrinth.hpp"
-
-const static Labyrinth<12> LABYRINTH ({
-
-	false, false, false, false, false, false, false, false, false, false, false, false, 
-	false, true, true, true, true, true, true, true, true, true, true, false, 
-	false, true, false, false, false, false, true, true, false, false, true, false, 
-	false, true, true, true, true, false, true, true, true, false, true, false, 
-	false, true, true, false, true, false, true, false, false, false, true, false, 
-	false, true, false, false, true, false, true, true, true, true, true, false, 
-	false, false, false, false, true, true, true, true, true, false, true, false, 
-	false, true, true, false, true, false, false, false, true, false, true, false, 
-	false, true, true, true, true, true, true, true, false, false, false, false, 
-	false, false, false, true, true, true, true, true, false, true, true, false, 
-	false, true, true, true, true, false, true, true, false, false, true, false, 
+    #include <array>
+    #include "labyrinth.hpp"
+    
+    const static Labyrinth<12> LABYRINTH ({
+    
+    	false, false, false, false, false, false, false, false, false, false, false, false, 
+	false, true, true, true, true, true, true, false, true, true, true, false, 
+	false, true, false, true, false, true, false, true, false, true, false, false, 
+	false, true, true, false, false, true, true, true, false, true, true, false, 
+	false, true, false, true, true, false, true, false, true, true, false, false, 
+	false, false, true, false, true, false, true, false, false, true, true, false, 
+	false, true, true, true, true, true, true, true, true, true, false, false, 
+	false, false, true, false, false, true, false, true, false, true, true, false, 
+	false, false, false, true, false, true, true, false, false, true, false, false, 
+	false, false, true, true, true, true, false, false, true, true, true, false, 
+	false, true, true, false, true, false, true, true, true, false, true, false, 
 	false, false, false, false, false, false, false, false, false, false, false, false
 });
